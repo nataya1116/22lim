f574e1b8baaaf80b83265b077a1d9b92d21a632f
nataya1116 <https://github.com/nataya1116/nodeJs>
2022-07-05 20:06:50 +0900

    uill.js 파일 생성 및 객체 생성 함수를 만들었음

---
+++ js/utill.js
@@ -0,0 +1,111 @@
+function createStuffObj(stuffData, ctx, save){
+    const stuffArr = [];
+
+    let name;
+    let info;
+    let x;
+    let y;
+    let width;
+    let height;
+    let itemName;
+    let itemInfo;
+    let hintMsg;
+
+    stuffData.forEach(el => {
+        name = el.name;
+        info = el.info;
+        x = el.x;
+        y = el.y;
+        width = el.width;
+        height = el.height;
+
+        el.properties.forEach(el2 => {
+            if(el2.name === "info"){
+                info = el2.value;
+            }
+            if(el2.name === "itemName"){
+                itemName = el2.value;
+            }
+            if(el2.name === "itemInfo"){
+                itemInfo = el2.value;
+            }
+            if(el2.name === "hintMsg"){
+                hintMsg = el2.value;
+            }
+        });
+
+        if(name === "구급함"){
+            stuffArr.push(new SavePoint({ctx, name, info, x, y, width, height, itemName, itemInfo}));
+        }else if(name === "엑스레이보드"){
+            stuffArr.push(new StuffHint({ctx, name, info, x, y, width, height, itemName, itemInfo, hintMsg}));
+        }
+        else{
+            stuffArr.push(new Stuff({ctx, name, info, x, y, width, height, itemName, itemInfo}));
+        }
+        console.log(el);
+    });
+    return stuffArr;
+}
+
+function createPortalObj(portalData){
+    const portalArr = [];
+
+    let name;
+    let info;
+    let x;
+    let y;
+    let width;
+    let height;
+    let itemName;
+    let itemInfo;
+
+    let isDead;
+    let isKeyboard;
+    let isPortal;
+    let nextStage;
+    let notAvailableMsg;
+    let pw;
+    
+    portalData.forEach(el => {
+        name = el.name;
+        info = el.info;
+        x = el.x;
+        y = el.y;
+        width = el.width;
+        height = el.height;
+
+        el.properties.forEach(el2 => {
+            if(el2.name === "info"){
+                info = el2.value;
+            }
+            if(el2.name === "itemName"){
+                itemName = el2.value;
+            }
+            if(el2.name === "itemInfo"){
+                itemInfo = el2.value;
+            }
+            if(el2.name === "isDead"){
+                isDead = el2.value;
+            }
+            if(el2.name === "isKeyboard"){
+                isKeyboard = el2.value;
+            }
+            if(el2.name === "isPortal"){
+                isPortal = el2.value;
+            }
+            if(el2.name === "nextStage"){
+                nextStage = el2.value;
+            }
+            if(el2.name === "notAvailableMsg"){
+                notAvailableMsg = el2.value;
+            }
+            if(el2.name === "pw"){
+                infoInfo = el2.value;
+            }
+            
+        });
+
+        portalArr.push(new Portal({ ctx, name, info, x, y, width, height, itemName, itemInfo, pw, isKeyboard, isPortal, isDead, nextStage, notAvailableMsg }));
+    });
+    return portalArr;
+}
